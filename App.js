3424e75e5a5097cbae2c722992aad8f2fb008cac
Anmol Shrestha <shrestha.anmol.as@gmail.com>
2020-07-14 04:15:46 -0400

    app with login/signup pages

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Providers from './src/navigation';
+import * as firebase from 'firebase';
+
+// Optionally import the services that you want to use
+//import "firebase/auth";
+//import "firebase/database";
+//import "firebase/firestore";
+//import "firebase/functions";
+//import "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: 'AIzaSyDCUsjfrCtgrLASBS2kB2BI4B7xQIiD9tE',
+  authDomain: 'hawk-chat-react-native.firebaseapp.com',
+  databaseURL: 'https://hawk-chat-react-native.firebaseio.com',
+  projectId: 'hawk-chat-react-native',
+  storageBucket: 'hawk-chat-react-native.appspot.com',
+  messagingSenderId: '608302673417',
+  appId: '1:608302673417:web:c2b77270d4098d505a12d8',
+  measurementId: 'G-TE950MBYEF',
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Providers />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
