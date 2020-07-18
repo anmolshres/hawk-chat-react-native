68f0b951b73b4c4259eccb32cac3a93ef107d5d6
Anmol Shrestha <shrestha.anmol.as@gmail.com>
2020-07-18 02:39:58 -0400

    createProfile function in utils

---
+++ src/utils/createProfile.js
@@ -0,0 +1,34 @@
+import firebaseApp from '../../firebase';
+
+export default function createProfile(
+  currentUser,
+  displayName,
+  year,
+  major,
+  hometown
+) {
+  return new Promise(function (resolve, reject) {
+    try {
+      firebaseApp
+        .firestore()
+        .collection('USERS')
+        .add({
+          userID: currentUser.uid,
+          email: currentUser.email,
+          displayName: displayName,
+          classYear: year,
+          major: major,
+          hometown: hometown,
+        })
+        .then(() => {
+          resolve();
+        })
+        .catch(() => {
+          reject();
+        });
+    } catch (e) {
+      console.log(e);
+      reject();
+    }
+  });
+}
