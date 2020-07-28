e107c18a9b72062452958c41badda16c3df51431
Anmol Shrestha <shrestha.anmol.as@gmail.com>
2020-07-28 02:52:14 -0400

    refactor logic to create private chats

--- src/components/PersonListItem.js
+++ src/components/PersonListItem.js
@@ -8,51 +8,40 @@ import generateHash from '../utils/generateHash';
 
 const { width, height } = Dimensions.get('screen');
 
-//Emjois yay 😅😅😅
 export const PersonListItem = ({ match, openProfile, navigation }) => {
   const { user } = useContext(AuthContext);
 
   function handleAddPrivateMessage(targetName) {
     if (targetName) {
       const hash = generateHash([targetName.email, user.email].sort().join(''));
-      firebaseApp
-        .firestore()
-        .collection('THREADS')
-        .where('hash', '==', hash)
-        .onSnapshot((snap) => {
-          const haveTalked = !snap.empty;
-          if (!haveTalked) {
-            getUserInfo(user.email).then((currUser) => {
-              firebaseApp
-                .firestore()
-                .collection('THREADS')
-                .add({
-                  name: `Private thread between ${currUser.email} and ${targetName.email}`,
-                  latestMessage: {
-                    text: `${currUser.displayName} started a conversation with ${targetName.displayName} 😇`,
-                    createdAt: new Date().getTime(),
-                  },
-                  participants: [currUser.email, targetName.email],
-                  type: 'private',
-                  hash: hash,
-                })
-                .then((docRef) => {
-                  docRef.collection('MESSAGES').add({
-                    text: `${currUser.displayName} started a conversation with ${targetName.displayName} 😇`,
-                    createdAt: new Date().getTime(),
-                    system: true,
-                  });
-                  navigation.navigate('Home');
-                });
-            });
-          } else {
-            const threadToNavigate = {
-              _id: snap.docs[0].id,
-              ...snap.docs[0].data(),
-            };
-            navigation.navigate('Room', { thread: threadToNavigate });
-          }
-        });
+      getUserInfo(user.email).then((currUser) => {
+        firebaseApp
+          .firestore()
+          .collection('THREADS')
+          .doc(hash.toString().concat('private'))
+          .set({
+            name: `Private thread between ${currUser.email} and ${targetName.email}`,
+            latestMessage: {
+              text: `${currUser.displayName} started a conversation with ${targetName.displayName}.`,
+              createdAt: new Date().getTime(),
+            },
+            participants: [currUser.email, targetName.email],
+            type: 'private',
+          })
+          .then(() => {
+            firebaseApp
+              .firestore()
+              .collection('THREADS')
+              .doc(hash.toString().concat('private'))
+              .collection('MESSAGES')
+              .add({
+                text: `${currUser.displayName} started a conversation with ${targetName.displayName}.`,
+                createdAt: new Date().getTime(),
+                system: true,
+              });
+            navigation.navigate('Home');
+          });
+      });
     }
   }
 
